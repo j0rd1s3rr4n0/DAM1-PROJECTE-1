5dfad29437d4306e7e7d3f5d6331d4b5dda8588e
Jordi Serrano <44474715+j0rd1s3rr4n0@users.noreply.github.com>
2022-02-08 16:13:34 +0100

    .

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.6/firebase-app.js";
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
 
