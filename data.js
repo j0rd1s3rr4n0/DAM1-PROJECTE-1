d0d949ece7a80b039eb17aef2b9eba4814341832
Jordi Serrano <44474715+j0rd1s3rr4n0@users.noreply.github.com>
2022-02-09 00:26:25 +0100

    .

--- data.js
+++ data.js
@@ -1,19 +1,21 @@
 
-url_img='https://rockcontent.com/es/wp-content/uploads/sites/3/2019/02/o-que-e-produto-no-mix-de-marketing-1024x538.png';
+let url_img='https://rockcontent.com/es/wp-content/uploads/sites/3/2019/02/o-que-e-produto-no-mix-de-marketing-1024x538.png';
 
-img_title='Titulo';
+let img_title='Titulo';
 
-id='1';
+let id='1';
 
-nom_producte='NomProducte';
+let nom_producte='NomProducte';
 
-descripcio = 'descripcio';
-
+let descripcio = 'descripcio';
+let stars = '<div class="rate"><input type="radio" id="star5" name="rate" value="5" /><label for="star5" title="text">5 stars</label><input type="radio" id="star4" name="rate" value="4" /><label for="star4" title="text">4 stars</label><input type="radio" id="star3" name="rate" value="3" /><label for="star3" title="text">3 stars</label><input type="radio" id="star2" name="rate" value="2" /><label for="star2" title="text">2 stars</label><input type="radio" id="star1" name="rate" value="1" /><label for="star1" title="text">1 star</label></div>';
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
-let fifi = '<div class="producto"><img src="'+url_img+'" class="producte" alt="'+img_title+'producte" id="'+id+'"><h3>'+nom_producte+'</h3><p>'+descripcio+'</p><div class="rate"><input type="radio" id="star5" name="rate" value="5" /><label for="star5" title="text">5 stars</label><input type="radio" id="star4" name="rate" value="4" /><label for="star4" title="text">4 stars</label><input type="radio" id="star3" name="rate" value="3" /><label for="star3" title="text">3 stars</label><input type="radio" id="star2" name="rate" value="2" /><label for="star2" title="text">2 stars</label><input type="radio" id="star1" name="rate" value="1" /><label for="star1" title="text">1 star</label></div></div>';
+
+let fifi = '<div class="producto"><img src="'+url_img+'" class="producte" alt="'+img_title+'producte" id="'+id+'"><h3>'+nom_producte+'</h3><p>'+descripcio+'</p></div>';
+
 for(let i = 0;i < 16;i++){
     contenedor = document.getElementById("prod");
     errcontenedor = document.createElement("div");
---
+++ js/config.js
@@ -0,0 +1,11 @@
+firebase.initializeApp({
+  apiKey: "AIzaSyBzvtHwBSTz1WntOeiu9c6At0TlC3Nx0pM",
+  authDomain: "test-59f9a.firebaseapp.com",
+  databaseURL: "https://test-59f9a-default-rtdb.firebaseio.com",
+  projectId: "test-59f9a",
+  storageBucket: "test-59f9a.appspot.com",
+  messagingSenderId: "788179962431",
+  appId: "1:788179962431:web:2dde0b49b63d257640c68a"
+});
+
+const auth = firebase.auth();
