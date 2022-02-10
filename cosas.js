3faa4b9533b8a6fd4bfb73451b30bab9166d2292
Jordi Serrano <44474715+j0rd1s3rr4n0@users.noreply.github.com>
2022-02-10 18:36:19 +0100

    .

--- cosas.js
+++ cosas.js
@@ -1,8 +1,14 @@
-  let coded = `<div class="col-md-3 col-sm-6 asd aqui">
+function Randomizador(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+  let imagen = "https://rockcontent.com/es/wp-content/uploads/sites/3/2019/02/o-que-e-produto-no-mix-de-marketing-1024x538.png";
+
+  function getCoded(imagen){
+      return `<div class="col-md-3 col-sm-6 asd aqui">
                     <div class="product-grid">
                         <div class="product-image">
                             <a href="#" class="image" style="background-color:#F3F3F3;">
-                            <img class="pic-1" src="https://rockcontent.com/es/wp-content/uploads/sites/3/2019/02/o-que-e-produto-no-mix-de-marketing-1024x538.png"><!--https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg-->
+                            <img class="pic-1" src="${imagen}"><!--https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg-->
                             </a>
                         </div>
                         <div class="product-content">
@@ -24,13 +30,18 @@
                         </div>
                     </div>
                 </div>`;
+  }
 
                 let i = 0;
                 let max = 23;
-                let code = coded;
+                let code = getCoded(imagen);
                 let t = false;
+                let imagenes = ["https://rockcontent.com/es/wp-content/uploads/sites/3/2019/02/o-que-e-produto-no-mix-de-marketing-1024x538.png","https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg"];
                 while(i<max){
-                     code+=coded;
+                    let ra = Randomizador(0,2);
+                    imagen = imagenes[ra];
+                    console.log(imagen);
+                     code+=getCoded(imagen);
                     i++;
                     if(i=>max){
                         t = true;
