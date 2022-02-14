878762403f253f4397e7498b904ca833af10b12c
Jordi Serrano <44474715+j0rd1s3rr4n0@users.noreply.github.com>
2022-02-14 20:52:25 +0100

    Update mixto.js

--- js/mixto.js
+++ js/mixto.js
@@ -3,6 +3,15 @@ import {addElement, getElement, onGetNew} from './firebase.js';
 
 const CAMBIARCODIGO = document.getElementById('change');
 
+// Set a Cookie
+function setCookie(cName, cValue, expDays) {
+        let date = new Date();
+        date.setTime(date.getTime() + (expDays * 24 * 60 * 60 * 1000));
+        const expires = "expires=" + date.toUTCString();
+        document.cookie = cName + "=" + cValue + "; " + expires + "; path=/";
+}
+// Apply setCookie
+setCookie('username', username, 30);
 
 
 
